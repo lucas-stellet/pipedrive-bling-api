1ec51ce1257481d3ca76c53a87fce6e67513297b
Lucas Stellet <lustepe@gmail.com>
2020-07-21 22:55:15 -0300

    add new route to api

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const deals = require('./components/deals').router
 const orders = require('./components/orders').router
+const dealsOrders = require('./components/deals-orders').router
 
 const router = Router()
 
 router.use('/pipedrive', deals)
 router.use('/bling', orders)
+router.use('/pipedrive-to-bling', dealsOrders)
 
 exports.router = router
