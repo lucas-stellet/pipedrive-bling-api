1d81e3006b1f32a9f915bb304e8bccb608567c15
krelod <pgomesrodrigues801@gmail.com>
2020-12-26 13:32:59 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DB_URI, {
@@ -17,17 +18,14 @@ mongoose.connect(process.env.DB_URI, {
 const { router } = require('./api')
 
 const app = express()
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT ? process.env.PORT : 3000
-
-app.use(express.json())
+app.use(bodyParser.json())
 
 app.use('/api', router)
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
+  if (err) throw err
 
-  return console.log(`Running on port ${port}...`)
+  console.log(`Running on port ${port}...`)
 })
